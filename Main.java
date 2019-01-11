adaa2786e679c48c09e31dc6d769ff58c6950fe6
anjali3107 <anjalidlive@gmail.com>
2019-01-11 17:46:26 +0530

    add star program

--- Main.java
+++ Main.java
@@ -1,10 +1,21 @@
 package com.company;
 
+import javax.security.sasl.SaslServer;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("hello world!");
         System.out.println("My first commit was successful");
+        System.out.println("Triangle");
+        for (int i=1;i<=4;i++)
+        {
+            for (int j=1;j<=i;j++)
+            {
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
     }
 }
